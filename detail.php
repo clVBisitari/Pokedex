de8065a2201c7febf3c944f485e6e156afaafde2
Mariano Sald <odioescribirel@hotmail.com>
2024-05-05 21:59:10 -0300

    Vista detalle lista. tmb division de footer

--- detail.php
+++ detail.php
@@ -1,7 +1,29 @@
-<!DOCTYPE html>
 <?php
 session_start();
-include ("DB.php");
+
+
+if([$_GET['id']] !== null){
+
+}
+
+function getPokemonById($id){
+    include("DB.php");
+
+    $query = "SELECT * FROM pokemon WHERE id = $id ";
+
+    $result = $connection->query($query);
+    if($obj = mysqli_fetch_assoc($result)){
+//        var_dump($obj);
+        return $obj;
+    }
+
+    return null;
+}
+
+?>
+<!DOCTYPE html>
+<?php
+
 $estaLogueado=isset($_SESSION["username"]);
 
 if (isset($_POST["submit"])) {
@@ -25,16 +47,19 @@ if (isset($_POST["submit"])) {
     }
 }
 ?>
+<?php
+$id = urldecode($_GET["id"]);
+$pokemon = getPokemonById($id); ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <title>Pokedex</title>
+    <title><?php echo $pokemon['name'] ?></title>
 </head>
 
-<body class="mb-0 bg-white text-black" >
+<body class="min-vh-100 mb-0 bg-white text-black" >
 <header class="container-fluid bg-primary bg-gradient text-white ">
     <div class="row">
         <div class=" bg-primary bg-gradient text white col text-left">
@@ -69,30 +94,42 @@ if (isset($_POST["submit"])) {
                 <?php
                 }else{
 
-                ?>
-                <p>Bienvenido, <?php echo $_SESSION["username"]; ?> </p>
+                    ?>
+                    <p>Bienvenido, <?php echo $_SESSION["username"]; ?> </p>
                     <form method="post" action="logout.php">
                         <button type="submit" class="btn btn-danger">Cerrar Sesión</button>
                     </form>
-                <?php
-            }
-            ?>
+                    <?php
+                }
+                ?>
             </div>
 
         </div>
     </div>
 </header>
-<div class="mt-4 mb-4 container text-center">
+<div class="mh-50 m-100 container text-center">
+
     <?php
-    include_once 'search.php';
+    echo "<h1 class='m-5'>" . $pokemon['name'] . "</h1>";
+    echo "<div class='d-flex flex-row m-5'>";
+    echo "<div class='w3-card-4 w3-left w3-blue w3-round-xxlarge p-1'>";
+    echo "<img src='" . $pokemon['image'] . "' alt='" . $pokemon['name'] . "' style='width:70%' class='w3-image w3-round-xxlarge w3-margin-top'>";
+//    echo "<img src='" . $pokemon['type1_image'] . "' alt='" . $pokemon['type1_name'] . "' style='width:30px; height:30px;' class='w3-round w3-margin-right'>";
+    echo "</div>";
+    echo "<div class='d-row'>";
+    echo "<p class = 'm-5 text-left'>". $pokemon["description"] . "</p>";
+    echo "</div>";
+    echo "</div>";
+    echo "<button type='submit' class='btn btn-sm btn-primary p-3 m-5'"."onclick="."document.location.href="."'index.php'".">" . "Volver" ."</button>";
     ?>
+
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <?php
-require_once("footer.php");
+ require_once("footer.php");
 ?>
 </body>
 </html>
